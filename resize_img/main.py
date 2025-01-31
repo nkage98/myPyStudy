b1f3a3d6a31e29e86ca0b4db2f30ea11abbb7edc
Lucas Kageyama <lnakamura.kage@gmail.com>
2025-01-30 20:03:10 -0800

    resize

---
+++ resize_img/main.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from pathlib import Path
+
+
+ROOT_FOLDER = Path(__file__).parent
+ORIGINAL = ROOT_FOLDER/'violet.png'
+RESIZED_IMAGE = ROOT_FOLDER/'new.jpg'
+
+p_image = Image.open(ORIGINAL)
+width, height = p_image.size
+new_width = 640
+new_height = round(height * new_width / width)
+
+new_image = p_image.resize(size=(new_width, new_height))
+new_image.save(
+    RESIZED_IMAGE,
+    optimize=True,
+    quality=70,
+    exit=exit,
+)
+
+
